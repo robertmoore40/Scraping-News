1997054a766157fc67e76f63fd0694b8319502cd
Robert Moore <rcmoore3@ncsu.edu>
2020-01-10 09:58:27 -0500

    added repo, dependencies, server, and npm packages

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
