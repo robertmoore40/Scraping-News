155abe925b778f4a8938cc27a6e0d9e9a4ada674
Robert Moore <rcmoore3@ncsu.edu>
2020-01-13 14:47:57 -0500

    added mongoose connection

--- server.js
+++ server.js
@@ -8,13 +8,32 @@ console.log("required packages loaded");
 
 // const db = require('./models');
 const PORT = process.env.PORT || 3000;
-
-const MONGOD_URI = process.env.MONGODB_URI;
 const app = express();
+
+// handlebars
+app.engine("handlebars",exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+// To be changed in class when we study mongoose more
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mongoosepractice',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("mongoose is connected")
+    
+  })
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoosepractice";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
 
 console.log('express, ports, and database loaded');
 // app.use(express.static('public'));
-
 
 axios.get("https://www.nytimes.com/").then(function(response) {
 
@@ -24,14 +43,9 @@ axios.get("https://www.nytimes.com/").then(function(response) {
     
       var result = [];
       
-    //   var headline = $(element).find("h2").text().trim();
-    //   var url = 'https://www.nytimes.com' + $(element).find("a").attr("href");
-    //   var summary = $(element).find("p").text().trim();
-      
       result.headline = $(element).find("h2").text().trim();
       result.url = 'https://www.nytimes.com' + $(element).find("a").attr("href");
       result.summary = $(element).find("p").text().trim();
-
 
       console.log(result)});
 
