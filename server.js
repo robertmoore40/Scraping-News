a36ed94934a99798e14dd6eb4aae4a4a434d3922
Robert Moore <rcmoore3@ncsu.edu>
2020-01-13 11:41:22 -0500

    scraper working

--- server.js
+++ server.js
@@ -4,3 +4,40 @@ const mongoose = require('mongoose');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+console.log("required packages loaded");
+
+// const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+const MONGOD_URI = process.env.MONGODB_URI;
+const app = express();
+
+console.log('express, ports, and database loaded');
+// app.use(express.static('public'));
+
+// app.get('/scrape', function(req,res){
+    console.log('scrape route reached');
+    axios.get('https://www.foxnews.com').then(function(response){
+    // cheerio time
+    var $ = cheerio.load(response.data);
+    var results = []
+    console.log('cheerio loaded')
+
+    $('li.related-item').each(function (i, element) {
+        var title = $(element).text();
+        var link = $(element).children().attr('href');
+
+    
+    results.push({
+        title: title,
+        link :link
+      })});
+    console.log("|||||||||||||||||||||||||||||||||||||||||||||||");
+    console.log(results);
+    // console.log(headline);
+    console.log("|||||||||||||||||||||||||||||||||||||||||||||||");
+    
+
+    });
+// });
+
